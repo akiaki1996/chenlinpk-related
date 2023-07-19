06fe70cc37b5937385f63d25e87d6ea7909bf75c
akiaki1996 <wqiuyang1996@outlook.com>
2023-07-19 10:57:59 +0800

    update 0715pk

--- lottery.py
+++ lottery.py
@@ -83,7 +83,121 @@ def photo():
         for row in lottery.items():
             writer.writerow(row)
 
+def basic30r():
+    total = 0
+    lottery = {}
+    memo = set()
+    df = pd.read_csv('./intermediate/0715_30_lottery.csv', encoding='utf-8', header = 0)
+    #print(df.head())
+    for row in df.iterrows():
+        print(row[1])
+        total = total + row[1]['numbers']
+    print(total)
+    for row in df.iterrows():
+        num = []
+        times = row[1]['numbers']
+        while(times > 0):
+            temp = random.randint(1, total + 1)
+            if temp not in memo:
+                memo.add(temp)
+                num.append(temp)
+                times = times - 1
+        
+        lottery[row[1]['id']] = num
+        
+    #print(lottery)
+    with open('./results/0715_30_numbers.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for row in lottery.items():
+            writer.writerow(row)
 
+def basic80r():
+    total = 0
+    lottery = {}
+    memo = set()
+    df = pd.read_csv('./intermediate/0715_80_lottery.csv', encoding='utf-8', header = 0)
+    #print(df.head())
+    for row in df.iterrows():
+        #print(row[1])
+        total = total + row[1]['numbers']
+    print(total)
+    for row in df.iterrows():
+        num = []
+        times = row[1]['numbers']
+        while(times > 0):
+            temp = random.randint(1, total + 1)
+            if temp not in memo:
+                memo.add(temp)
+                num.append(temp)
+                times = times - 1
+        
+        lottery[row[1]['id']] = num
+        
+    #print(lottery)
+    with open('./results/0715_80_numbers.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for row in lottery.items():
+            writer.writerow(row)
+
+
+
+def basic150r():
+    total = 0
+    lottery = {}
+    memo = set()
+    df = pd.read_csv('./intermediate/0715_150_lottery.csv', encoding='utf-8', header = 0)
+    #print(df.head())
+    for row in df.iterrows():
+        #print(row[1])
+        total = total + row[1]['numbers']
+    print(total)
+    for row in df.iterrows():
+        num = []
+        times = row[1]['numbers']
+        while(times > 0):
+            temp = random.randint(1, total + 1)
+            if temp not in memo:
+                memo.add(temp)
+                num.append(temp)
+                times = times - 1
+        
+        lottery[row[1]['id']] = num
+        
+    #print(lottery)
+    with open('./results/0715_150_numbers.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for row in lottery.items():
+            writer.writerow(row)
+
+
+
+def basic300r():
+    total = 0
+    lottery = {}
+    memo = set()
+    df = pd.read_csv('./intermediate/0715_300_lottery.csv', encoding='utf-8', header = 0)
+    #print(df.head())
+    for row in df.iterrows():
+        #print(row[1])
+        total = total + row[1]['numbers']
+    print(total)
+    for row in df.iterrows():
+        num = []
+        times = row[1]['numbers']
+        while(times > 0):
+            temp = random.randint(1, total + 1)
+            if temp not in memo:
+                memo.add(temp)
+                num.append(temp)
+                times = times - 1
+        
+        lottery[row[1]['id']] = num
+        
+    #print(lottery)
+    with open('./results/0715_300_numbers.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for row in lottery.items():
+            writer.writerow(row)
 
 if __name__=='__main__':
-    photo()
+    basic300r()
